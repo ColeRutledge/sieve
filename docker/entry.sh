d3e364238f12cf8ad456d20225dcc516eb3c5908
Cole Rutledge <ColeRutledge@gmail.com>
2022-07-17 19:39:23 -0400

    :toolbox: circleci wip

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,3 +1,9 @@
-#! /bin/sh
+#! /bin/bash
+
+# Bash "strict mode", to help catch problems and bugs in the shell
+# script. Every bash script you write should include this. See
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/ for
+# details.
+set -euo pipefail
 
 cron -f
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -9,16 +9,32 @@ from tasks.console import Text, console
 monkeypatch.fix_annotations()
 
 
+@task(aliases=["du"])
+def docker_up(c: Context) -> None:
+    # TODO: add cross platform path handling
+    console.log(f"{' DEV STARTUP ':~^50}", style="green")
+    compose_file = "-f .\docker\docker-compose.yml"
+    c.run(f"docker-compose {compose_file} up -d --build")
+
+
+@task(aliases=["dd"])
+def docker_down(c: Context) -> None:
+    # TODO: add cross platform path handling
+    console.log(f"{'DEV SHUTDOWN':~^50}", style="red")
+    compose_file = "-f .\docker\docker-compose.yml"
+    c.run(f"docker-compose {compose_file} down")
+
+
 @task(aliases=["hi"])
 def hooks_install(c: Context) -> None:
     console.print("Installing hooks...", style="bold green")
-    c.run("poetry run pre-commit install")
+    c.run("pre-commit install")
 
 
 @task(aliases=["hr"])
 def hooks_run(c: Context) -> None:
     console.print("Running hooks...", style="bold green")
-    c.run("poetry run pre-commit run --all-files")
+    c.run("pre-commit run --all-files")
 
 
 @task(hooks_install, hooks_run, aliases=["h"])
@@ -31,7 +47,7 @@ def black(c: Context) -> None:
     console.print("Running black...", style="bold green")
     output = io.StringIO()
     c.run(
-        command="poetry run black . --diff",
+        command="black . --diff",
         out_stream=output,
         err_stream=output,
         encoding="utf-8",
@@ -44,7 +60,7 @@ def black(c: Context) -> None:
     if should_apply:
         output = io.StringIO()
         c.run(
-            command="poetry run black .",
+            command="black .",
             out_stream=output,
             err_stream=output,
             encoding="utf-8",
