4f6e18141a37256a2e1ba923eeff3d9034cab083
Cole Rutledge <ColeRutledge@gmail.com>
2022-07-30 11:00:13 -0400

    :whale: add debugpy compose

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,7 +1,5 @@
 import io
 
-from pathlib import Path
-
 from invoke import Context, task
 
 from tasks import monkeypatch
@@ -14,26 +12,17 @@ monkeypatch.fix_annotations()
 @task(aliases=["dev", "up"])
 def developer_up(c: Context, all: bool = False) -> None:
     console.print(f"{' DEV STARTUP ':~^75}", style="green")
-    project_root = Path().absolute()
-    c.run("docker compose -f ./docker/docker-compose.yml build sieve db browserless")
     if all:
-        c.run("docker-compose -f ./docker/docker-compose.yml --profile primary up -d")
+        c.run("docker-compose -f ./docker/docker-compose.yml up -d --build")
     else:
-        # default: run app as individual container with debugpy waiting to connect to debugger
-        c.run("docker-compose -f ./docker/docker-compose.yml --profile support up -d")
+        # default -> run with debugpy waiting to connect to debugger
         c.run(
-            "docker run "
-            "--tty "
-            "--detach "
-            "--name sieve "
-            "--publish 5678:5678 "
-            "--network=sieve "
-            "--restart unless-stopped "
-            f'--mount type=bind,source="{project_root}",target=/sieve '
-            "--env-file=.env "
-            '--entrypoint="" '
-            "sieve:dev "
-            '"sh" "-c" "pip install debugpy -t /tmp && python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 -m sieve.__main__"'
+            "docker-compose "
+            "-f ./docker/docker-compose.yml "
+            "-f ./docker/docker-compose.debugpy.yml "
+            "up "
+            "-d "
+            "--build"
         )
 
 
