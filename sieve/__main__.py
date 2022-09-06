bae50e5793bdb820b59f3c06b784153ff6deae94
Cole Rutledge <ColeRutledge@gmail.com>
2022-09-06 09:18:31 -0400

    :fire: remove main test

--- sieve/__main__.py
+++ sieve/__main__.py
@@ -1,16 +1,11 @@
-from sieve.logger import DATADOG_LOG_CLIENT, get_logger
+from sieve.logger import get_logger
 
 
 logger = get_logger(__name__)
 
 
 def main():
-    try:
-        logger.info("MESSAGE")
-
-    finally:
-        if DATADOG_LOG_CLIENT:
-            DATADOG_LOG_CLIENT.close()
+    logger.info("MESSAGE")
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,22 +1,22 @@
-from unittest.mock import Mock
+# from unittest.mock import Mock
 
-from pytest import LogCaptureFixture, MonkeyPatch
+# from pytest import LogCaptureFixture, MonkeyPatch
 
-from sieve.__main__ import main
+# from sieve.__main__ import main
 
 
-def test_main(caplog: LogCaptureFixture, monkeypatch: MonkeyPatch, enable_logging):
-    class MockDatadogLogClient:
-        def close(self):
-            pass
+# def test_main(caplog: LogCaptureFixture, monkeypatch: MonkeyPatch, enable_logging):
+#     class MockDatadogLogClient:
+#         def close(self):
+#             pass
 
-    mock_client = MockDatadogLogClient()
-    # https://github.com/python/mypy/issues/2427#issuecomment-929688736
-    mock_client.close = Mock()  # type: ignore
-    monkeypatch.setattr("sieve.__main__.logger.propagate", True)
-    monkeypatch.setattr("sieve.__main__.DATADOG_LOG_CLIENT", mock_client)
+#     mock_client = MockDatadogLogClient()
+#     # https://github.com/python/mypy/issues/2427#issuecomment-929688736
+#     mock_client.close = Mock()  # type: ignore
+#     monkeypatch.setattr("sieve.__main__.logger.propagate", True)
+#     monkeypatch.setattr("sieve.__main__.DATADOG_LOG_CLIENT", mock_client)
 
-    main()
+#     main()
 
-    assert "MESSAGE\n" in caplog.text
-    mock_client.close.assert_called_once()
+#     assert "MESSAGE\n" in caplog.text
+#     mock_client.close.assert_called_once()
