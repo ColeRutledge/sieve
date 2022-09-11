a43894cf6181b08b765c99284328fcebed586a1b
Cole Rutledge <ColeRutledge@gmail.com>
2022-09-11 14:43:15 -0400

    :toolbox: fix protocols and update types

--- tests/services/test_driver.py
+++ tests/services/test_driver.py
@@ -2,8 +2,7 @@ from pytest import LogCaptureFixture, MonkeyPatch, raises
 from selenium.common.exceptions import NoSuchElementException
 
 from sieve.exceptions import SystemException
-from sieve.logger import get_logger
-from sieve.services.driver import Driver, init_driver
+from sieve.services.driver import DriverProtocol, init_driver
 from tests.mocks import MockRemote
 
 
@@ -29,12 +28,12 @@ def test_driver_init_for_prod_config(monkeypatch: MonkeyPatch, prod_settings):
     assert driver.driver.command_executor == "http://browserless:3000/webdriver"
 
 
-def test_driver_xpath_returns_single_web_element(dev_driver: Driver):
+def test_driver_xpath_returns_single_web_element(dev_driver: DriverProtocol):
     element = dev_driver.element("//*[text()='mock xpath']")
     assert element.text == "mock xpath"
 
 
-def test_driver_xpath_raises_exception_when_element_not_found_in_time(dev_driver: Driver):
+def test_driver_xpath_raises_exception_when_element_not_found_in_time(dev_driver: DriverProtocol):
     setattr(dev_driver.driver, "find_elements", lambda *args, **kwargs: [])
     setattr(dev_driver, "save_screenshot", lambda *args, **kwargs: True)
 
@@ -45,26 +44,23 @@ def test_driver_xpath_raises_exception_when_element_not_found_in_time(dev_driver
 
 
 def test_driver_get_logs_request_url(
-    dev_driver: Driver,
+    dev_driver: DriverProtocol,
     caplog: LogCaptureFixture,
     enable_logging,
 ):
-    # logging.disable breaks pytest ouput capturing
-    get_logger("sieve.services.driver")
-
     dev_driver.get("https://test-url.test")
     dev_driver.quit()
 
     assert "[driver] GET: https://test-url" in caplog.text
 
 
-def test_driver_save_screenshot_returns_true_on_success(dev_driver: Driver):
+def test_driver_save_screenshot_returns_true_on_success(dev_driver: DriverProtocol):
     assert dev_driver.driver.get_window_size() == {"width": 1200, "height": 800}
     assert dev_driver.save_screenshot("test.png") is True
     assert dev_driver.driver.get_window_size() == {"width": 1200, "height": 800}
 
 
-def test_driver_save_screenshot_returns_false_on_exception(dev_driver: Driver):
+def test_driver_save_screenshot_returns_false_on_exception(dev_driver: DriverProtocol):
     def mock_element(*args, **kwargs):
         raise SystemException("ERROR")
 
